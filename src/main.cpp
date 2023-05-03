3b7abe708277e7df838e70a9f0d887996aba97fc
Roman-Bond3D <r.vanderweerd@bond3d.com>
2023-05-03 18:25:32 +0200

    RWE

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,27 @@ const int
     ESTOP1_PIN = 12,
     ESTOP2_PIN = 13;
 
-AccelStepper stepper(AccelStepper::DRIVER, 2, 5, true);
+AccelStepper stepper(AccelStepper::DRIVER, 2, 5);
 
 const bool emergencyStop()
 {
    const bool eStop1 = digitalRead(ESTOP1_PIN);
    const bool eStop2 = digitalRead(ESTOP1_PIN);
 
-   if (eStop1 && eStop2)
+   if (!eStop1 && !eStop2)
    {
       return true;
-   } else 
+   }
+   else
    {
       return false;
    }
 }
 
-void stepperSetup()
+void emergencySetup()
 {
-   stepper.setEnablePin(8);
-   // stepper.setPinsInverted(false, false, true);
-   stepper.setAcceleration(750);
-   stepper.setMaxSpeed(1000);
-   stepper.setSpeed(500);
+   pinMode(ESTOP1_PIN, INPUT_PULLUP);
+   pinMode(ESTOP2_PIN, INPUT_PULLUP);
 }
 
 void stepperRun(const bool run)
@@ -41,15 +39,24 @@ void stepperRun(const bool run)
    }
 }
 
+void stepperSetup()
+{
+   stepper.setAcceleration(500);
+   stepper.setMaxSpeed(1000);
+   stepper.setSpeed(500);
+}
+
 void setup()
 {
-   pinMode(ESTOP1_PIN, INPUT);
-   pinMode(ESTOP2_PIN, INPUT);
-
+   Serial.begin(9600);
+   emergencySetup();
    stepperSetup();
 }
 
 void loop()
-{   
-   stepperRun(emergencyStop());
+{
+   const bool run = emergencyStop();
+   stepperRun(run);
+
+   Serial.println(run);
 }
